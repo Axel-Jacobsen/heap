5fb200891b1bc6d4941fb60a0d6b16cdb1e1f5b1
Axel-Jacobsen <axelnj44@gmail.com>
2018-02-15 21:24:34 -0800

    Start on db

---
+++ scripts/db.js
@@ -0,0 +1,16 @@
+const Dexie = require('dexie');
+
+class DB {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+      }
+}
+db = new Dexie('main');
+
+// Define a schema
+db.version(1).stores({
+    friends: 'name, age',
+});
+
+export default db;
