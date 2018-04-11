39521e458ca2c69753cabbe651c54b2a84b4c40c
Axel-Jacobsen <axelnj44@gmail.com>
2018-04-11 09:53:05 -0700

    mas

--- views/Item.jsx
+++ views/Item.jsx
@@ -1,19 +1,67 @@
 'use babel'
 
-const React = require('react');
+const React = require('react')
 
 /*
     Class for each individual item
 */
-
-export default class ItemTable extends React.Component {
+export default class Item extends React.Component {
     constructor(props) {
         super(props)
+        this.handleClick = this.handleClick.bind(this)
+        this.setWrapperRef = this.setWrapperRef.bind(this)
+        this.handleClickOutside = this.handleClickOutside.bind(this)
+        this.handleKeyPress = this.handleKeyPress.bind(this)
+
+        this.state = {
+            isClicked: false
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('mousedown', this.handleClickOutside)
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.handleClickOutside)
+    }
+
+    setWrapperRef(node) {
+        this.wrapperRef = node
+    }
+
+    handleClick(e) {
+        e.preventDefault()
+        this.setState(prevState => ({
+            isClicked: !prevState.isClicked
+        }))
+    }
+
+    handleClickOutside(event) {
+        if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+            this.setState(prevState => ({
+                isClicked: false
+            }))
+        }
+    }
+
+    handleKeyPress(event) {
+        console.log('FKC');
+        
+        if (event.key == 'Enter') {
+            alert('enter press here! ')
+        }
     }
 
     render() {
+        let itemClasses = "horizontal item"
+
+        if (this.state.isClicked) {
+            itemClasses += " item-active"
+        }
+
         return (
-            <div className="horizontal item">
+            <div className={itemClasses} onClick={this.handleClick} ref={this.setWrapperRef} onKeyPress={this.handleKeyPress}>
                 <div className="name"> {this.props.value.name}</div>
                 <div className="description">{this.props.value.description}</div>
                 <div className="date">
